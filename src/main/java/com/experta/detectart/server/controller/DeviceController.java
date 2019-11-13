9e22045c9e8e637fc227308870550b8b40efd83f
Andrés Butelman <abutelman@gmail.com>
2019-11-13 20:11:11 -0300

    Cambio de verbo de PUT a PATCH para renombrar el alias de un Device

--- src/main/java/com/experta/detectart/server/controller/DeviceController.java
+++ src/main/java/com/experta/detectart/server/controller/DeviceController.java
@@ -10,9 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -77,7 +77,7 @@ public class DeviceController {
         }).orElseThrow(() -> new ResourceNotFoundException("UserId " + userId + " not found"));
     }
 
-    @PutMapping(USERS + "/{userId}" + DEVICES + "/{macAddress}")
+    @PatchMapping(USERS + "/{userId}" + DEVICES + "/{macAddress}")
     public Device updateDevice(@PathVariable (value = "userId") final String userId,
                                  @PathVariable (value = "macAddress") final String macAddress,
                                  @Valid @RequestBody final Device deviceRequest) {
